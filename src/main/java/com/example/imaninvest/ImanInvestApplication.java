053bef25730fee5733e2097f54ed4d2d282bdc3b
1mmortal010 <bekayunusov948@gmail.com>
2023-03-27 20:25:30 +0500

    First commit

---
+++ src/main/java/com/example/imaninvest/ImanInvestApplication.java
@@ -0,0 +1,13 @@
+package com.example.imaninvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImanInvestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImanInvestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imaninvest/ImanInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imaninvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImanInvestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

a747fe86a415382be7dfea10c9cd157e95dd4ed7
Aliissomeoneelse <aliwulug1307@gmail.com>
2023-03-29 01:04:47 +0500

    class Goals added

---
+++ src/main/java/com/example/imaninvest/module/Goals.java
@@ -0,0 +1,13 @@
+package com.example.imaninvest.module;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.beans.IntrospectionException;
+
+@Getter
+@Setter
+public class Goals {
+    private Integer id;
+    private String text;
+}

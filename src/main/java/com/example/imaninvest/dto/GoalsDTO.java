e40f9a96c24be7f43f4178915df79bda8d4d817d
Aliissomeoneelse <aliwulug1307@gmail.com>
2023-03-29 15:27:03 +0500

    GoalsDTO was added

---
+++ src/main/java/com/example/imaninvest/dto/GoalsDTO.java
@@ -0,0 +1,11 @@
+package com.example.imaninvest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GoalsDTO {
+    private Integer id;
+    private String text;
+}
